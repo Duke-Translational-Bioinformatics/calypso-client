e28aedef0e8ac066d14f379ed41c85e79b13cb16
tom1193 <thomas8289@gmail.com>
2016-05-29 14:16:21 -0400

    Removing sidebar tests.

--- e2e/sidebar/sidebar.spec.js
+++ e2e/sidebar/sidebar.spec.js
@@ -10,20 +10,20 @@ describe('Sidebar directive', function () {
     Sidebar = require('./sidebar.po');
   });
 
-  it('sidebar should exist', function () {
-    expect(Sidebar.sidebarSection.isPresent()).toBe(true);
-    expect(Sidebar.findButton.isPresent()).toBe(true);
-    expect(Sidebar.loadButton.isPresent()).toBe(true);
-    expect(Sidebar.downloadButton.isPresent()).toBe(true);
-  });
+  // it('sidebar should exist', function () {
+  //   expect(Sidebar.sidebarSection.isPresent()).toBe(true);
+  //   expect(Sidebar.findButton.isPresent()).toBe(true);
+  //   expect(Sidebar.loadButton.isPresent()).toBe(true);
+  //   expect(Sidebar.downloadButton.isPresent()).toBe(true);
+  // });
 
-  it('buttons should be clickable', function () {
-    Sidebar.FAB.click();
-    Sidebar.findButton.click();
-    //    Sidebar.downloadButton.click();
-    Sidebar.loadButton.click();
+  // it('buttons should be clickable', function () {
+  //   Sidebar.FAB.click();
+  //   Sidebar.findButton.click();
+  //   //    Sidebar.downloadButton.click();
+  //   Sidebar.loadButton.click();
 
-    expect(true).toBe(true);
-  });
+  //   expect(true).toBe(true);
+  // });
 
 });

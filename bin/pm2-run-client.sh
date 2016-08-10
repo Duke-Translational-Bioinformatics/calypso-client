c89aebe0148aba4996fc79d33a0253fcebb4d6f2
Keegan Brown <brown.keegan@gmail.com>
2016-08-10 16:23:35 -0500

    deploy scripts working

--- bin/pm2-run-client.sh
+++ bin/pm2-run-client.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd calypso-client
 grunt serve:dist
--- bin/sb-staging-deploy.sh
+++ bin/sb-staging-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh deployer@calypso-qa.sbox.es 'cd calypso-client && git fetch --all && git reset --hard origin/feature/ie11-support && pm2 restart all'
+ssh deployer@calypso-qa.sbox.es 'cd calypso-client && git fetch --all && git reset --hard origin/feature/ie11-support && pm2 restart pm2-run-client'
